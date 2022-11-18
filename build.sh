d64ecc33bff938c0a833d86fe5e590c8264e1a4b
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-11-18 09:58:09 +0800

    Changing to use mvnw instead of mvn

--- build.sh
+++ build.sh
@@ -2,25 +2,25 @@
 export API_DIR=$HOME/gitsandbox/yugastore-k8s/src
 echo "API_DIR:$HOME"
 cd $API_DIR/api-gateway
-mvn clean package -DskipTests
+./mvnw clean package -DskipTests
 cp target/*.jar $API_DIR/target
 
 cd $API_DIR/eureka-server-local
-mvn clean package -DskipTests
+./mvnw clean package -DskipTests
 cp target/*.jar $API_DIR/target
 
 cd $API_DIR/products
-mvn clean package -DskipTests
+./mvnw clean package -DskipTests
 cp target/*.jar $API_DIR/target
 
 cd $API_DIR/cart
-mvn clean package -DskipTests
+./mvnw clean package -DskipTests
 cp target/*.jar $API_DIR/target
 
 cd $API_DIR/checkout
-mvn clean package -DskipTests
+./mvnw clean package -DskipTests
 cp target/*.jar $API_DIR/target
 
 cd $API_DIR/react-ui
-mvn clean package -DskipTests
+./mvnw clean package -DskipTests
 cp target/*.jar $API_DIR/target
